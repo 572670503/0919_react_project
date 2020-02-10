a4906aac1e12a5d9688fe34db422bd2e972d26fa
icookie <572670503@qq.com>
2020-02-11 01:20:28 +0800

    半夜提交

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,59 @@
 import React, { Component } from 'react'
-
-export default class Login extends Component {
+import { Form, Icon, Input, Button,Checkbox} from 'antd';
+import logo from "./img/logo.png"
+import "./css/login.less"
+const {Item}=Form
+class Login extends Component {
     render() {
+        const { getFieldDecorator } = this.props.form;
         return (
-            <h1>
-                Login
-            </h1>
+            <div id="login">
+                <div className="header">
+                    <img src={logo} alt="图片"/>
+                    <h1>商品管理系统</h1>
+                </div> 
+                <div className="content">
+                 <h1>用户登录</h1>
+                 <Form onSubmit={this.handleSubmit} className="login-form">
+                    <Item>
+                    {getFieldDecorator('password', {
+            rules: [{ required: true, message: '请输入用户名!' },
+                    {max:12,message:"用户名必须小于等于12位"},
+                    {min:4,message:"用户名必须大于等于4位"}, 
+                    {pattern:/^\w+$/,message:"用户名必须是英文、数字或下划线组成"}
+        
+                   ],
+          })(
+
+
+
+
+                        <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="用户名"
+                            />,
+                         )}
+                    </Item>
+                
+
+
+
+                    <Item>
+                        <Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="密码"
+                            />,
+                    </Item>
+                    <Item>
+                        <Button type="primary" htmlType="submit" className="login-form-button">
+                            登录
+                        </Button>
+                    </Item>
+                 </Form>
+                </div>
+            </div>
         )
     }
 }
+export default Form.create()(Login); 
