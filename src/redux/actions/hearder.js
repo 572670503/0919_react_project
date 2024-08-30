7a1cd5e474f1e7d9c079d4305501394d4dec409e
fuyu <fuyu92job@163.com>
2024-08-30 23:50:28 +0800

    提交测试

--- src/redux/actions/hearder.js
+++ src/redux/actions/hearder.js
@@ -1,7 +1,7 @@
 import {SAVE_HEADER_TITLE,DELETE_HEADER_TITLE} from '../action_types'
 
 export const createSaveTitleAction = (title)=> ({type:SAVE_HEADER_TITLE,data:title})
-
+console.log('测试')
 
 export const createDeleteTitleAction = ()=> ({type:DELETE_HEADER_TITLE,data:''})
 	
